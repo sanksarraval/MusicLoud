a19c4efab594f9c8465e604cdfe3d048b95f2039
logandoran <doranl1@myumanitoba.ca>
2023-02-07 11:52:44 -0600

    Created Song Object and Song Persistence Class

---
+++ app/src/main/java/com/example/musicloud/objects/Song.java
@@ -0,0 +1,25 @@
+package com.example.musicloud.objects;
+
+import java.io.*;
+
+public class Song {
+    private final String songName;
+    private final String artist;
+    private final String albumName;
+
+    //private final File mp3;
+    //private final File albumArt;
+
+    public Song(String songName, String artist) {
+        this.songName = songName;
+        this.artist = artist;
+        albumName = "na"; //
+    }
+
+    public Song (String songName, String artist, String albumName) {
+        this.songName = songName;
+        this.artist = artist;
+        this.albumName = albumName;
+    }
+
+}
