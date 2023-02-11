c01249522e5c08a95d9b7fb7f1a8dcb7d3af74cf
logandoran <doranl1@myumanitoba.ca>
2023-02-11 17:18:58 -0600

    Replaced SongList class with song persistence

--- app/src/main/java/com/example/musicloud/persistence/SongPersistence.java
+++ app/src/main/java/com/example/musicloud/persistence/SongPersistence.java
@@ -6,11 +6,18 @@ import com.example.musicloud.objects.Song;
 
 public interface SongPersistence {
 
-    List<Song> getSongs();
+    List<Song> getAllSongs();
+    //alpha order by artist then song
+
+    Song getSong (int i);
+
+    int getSize ();
 
     Song insertSong (Song currentSong);
 
     Song updateSong (Song currentSong);
 
     void deleteSong (Song currentSong);
+
+    List<String> allSongNames();
 }
