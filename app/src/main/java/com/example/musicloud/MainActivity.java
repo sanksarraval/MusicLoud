4cc0e2a35d578d9a45c8fb01ba3e026188992807
XIANFENG LANG <langx@myumanitoba.ca>
2023-02-12 19:01:55 +0000

    Revert "fix play bottom"
    This reverts commit 5fe915d20ae4f3eb8974213b63e4a40698e7b4bc

--- app/src/main/java/com/example/musicloud/MainActivity.java
+++ app/src/main/java/com/example/musicloud/MainActivity.java
@@ -2,10 +2,6 @@ package com.example.musicloud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-import android.app.Activity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -13,10 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.musicloud.presentation.LoginActivity;
 
-
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     EditText username, password;
     Button loginBtn;
@@ -30,8 +24,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        /*
         username = findViewById(R.id.username);
         password = findViewById(R.id.Password);
         loginBtn = findViewById(R.id.button);
@@ -40,25 +32,26 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
                 // Validating Inputs
-                if (TextUtils.isEmpty(username.getText().toString()) || TextUtils.isEmpty(password.getText().toString())) {
-                    Toast.makeText(MainActivity.this, "Please input Username and password!", Toast.LENGTH_LONG).show();
-                } else if (username.getText().toString().equals(correct_username)) {
-                    if (password.getText().toString().equals(correct_password)) {
-                        Toast.makeText(MainActivity.this, "Login was successful", Toast.LENGTH_LONG).show();
-                        startActivity(new Intent(MainActivity.this, PlayActivity.class));
-                    } else {
-                        Toast.makeText(MainActivity.this, "Incorrect Password!", Toast.LENGTH_LONG).show();
+                if(TextUtils.isEmpty(username.getText().toString())|| TextUtils.isEmpty(password.getText().toString()))
+                {
+                    Toast.makeText(MainActivity.this,"Please input Username and password!", Toast.LENGTH_LONG).show();
+                } else if (username.getText().toString().equals(correct_username))
+                {
+                    if(password.getText().toString().equals(correct_password))
+                    {
+                        Toast.makeText(MainActivity.this,"Login was successful", Toast.LENGTH_LONG).show();
                     }
-                } else {
-                    Toast.makeText(MainActivity.this, "Invalid Username/password!", Toast.LENGTH_LONG).show();
+                    else
+                    {
+                        Toast.makeText(MainActivity.this,"Incorrect Password!", Toast.LENGTH_LONG).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Invalid Username/password!", Toast.LENGTH_LONG).show();
                 }
             }
         });
-        */
-    }
-    public void buttonLoginOnClick(View v)
-    {
-        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-        MainActivity.this.startActivity(loginIntent);
+
     }
 }
