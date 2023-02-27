4e411104be96436755ae06c8b96d7827b19fe7b2
logandoran <doranl1@myumanitoba.ca>
2023-02-27 15:07:27 -0600

    Updated AccessSongs class and is ready to implement

--- app/src/main/java/com/example/musicloud/application/Services.java
+++ app/src/main/java/com/example/musicloud/application/Services.java
@@ -1,12 +1,15 @@
 package com.example.musicloud.application;
 
+import com.example.musicloud.persistence.SongPersistence;
 import com.example.musicloud.persistence.UserManagement;
+import com.example.musicloud.persistence.stubs.SongPersistenceStub;
 import com.example.musicloud.persistence.stubs.UserManagementStub;
 
 public class Services {
 
     private static UserManagement accountManagement = null;
 
+    private static SongPersistence songPersistence = null;
     public static synchronized UserManagement getAccountManagement()
     {
         if (accountManagement == null)
@@ -14,6 +17,15 @@ public class Services {
             accountManagement = new UserManagementStub();
         }
         return accountManagement;
+    }
+
+    public static synchronized SongPersistence getSongPersistence ()
+    {
+        if (songPersistence == null)
+        {
+            songPersistence = new SongPersistenceStub();
+        }
+        return songPersistence;
     }
 
 }
--- app/src/main/java/com/example/musicloud/business/AccessSongs.java
+++ app/src/main/java/com/example/musicloud/business/AccessSongs.java
@@ -1,4 +1,40 @@
 package com.example.musicloud.business;
 
+import com.example.musicloud.application.Services;
+import com.example.musicloud.objects.Song;
+import com.example.musicloud.persistence.SongPersistence;
+
+import java.util.Collections;
+import java.util.List;
+
 public class AccessSongs {
+    private SongPersistence songPersistence;
+    private List<Song> songs;
+    private Song song;
+    private int currentSong;
+
+    public AccessSongs(){
+        songPersistence = Services.getSongPersistence();
+        songs = null;
+        song = null;
+        currentSong = 0;
+    }
+
+    public List<Song> getSongs(){
+        songs = songPersistence.getAllSongs();
+        return Collections.unmodifiableList(songs);
+    }
+
+    public Song insertSong(Song currentSong){
+        return songPersistence.insertSong(currentSong);
+    }
+
+    public Song updateSong (Song currentSong){
+        return songPersistence.updateSong(currentSong);
+    }
+
+    public void deleteSong(Song currentSong){
+        songPersistence.deleteSong(currentSong);
+    }
+    
 }
