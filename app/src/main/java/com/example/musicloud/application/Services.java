33b5b60e8760f8955064cf1fec63741ac0e15580
logandoran <doranl1@myumanitoba.ca>
2023-03-02 10:43:00 -0600

    build access to Sp and playlist

--- app/src/main/java/com/example/musicloud/application/Services.java
+++ app/src/main/java/com/example/musicloud/application/Services.java
@@ -1,7 +1,11 @@
 package com.example.musicloud.application;
 
+import com.example.musicloud.persistence.PlaylistPersistence;
+import com.example.musicloud.persistence.SPPersistence;
 import com.example.musicloud.persistence.SongPersistence;
 import com.example.musicloud.persistence.UserManagement;
+import com.example.musicloud.persistence.stubs.PlaylistPersistenceStub;
+import com.example.musicloud.persistence.stubs.SPPersistenceStub;
 import com.example.musicloud.persistence.stubs.SongPersistenceStub;
 import com.example.musicloud.persistence.stubs.UserManagementStub;
 
@@ -10,6 +14,10 @@ public class Services {
     private static UserManagement accountManagement = null;
 
     private static SongPersistence songPersistence = null;
+
+    private static SPPersistence spPersistence = null;
+
+    private static PlaylistPersistence playlistPersistence = null;
     public static synchronized UserManagement getAccountManagement()
     {
         if (accountManagement == null)
@@ -26,6 +34,24 @@ public class Services {
             songPersistence = new SongPersistenceStub();
         }
         return songPersistence;
+    }
+
+    public static synchronized SPPersistence getSpPersistence ()
+    {
+        if (spPersistence == null)
+        {
+            spPersistence = new SPPersistenceStub();
+        }
+        return spPersistence;
+    }
+
+    public static synchronized PlaylistPersistence getPlaylistPersistence ()
+    {
+        if (playlistPersistence == null)
+        {
+            playlistPersistence = new PlaylistPersistenceStub();
+        }
+        return playlistPersistence;
     }
 
 }
