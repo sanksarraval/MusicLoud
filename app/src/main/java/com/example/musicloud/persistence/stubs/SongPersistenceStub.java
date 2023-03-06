3a65c0a14a33517d3d9a88d4992e1a3432fdecd8
logandoran <doranl1@myumanitoba.ca>
2023-03-06 11:06:01 -0600

    update SongPersistenceStub.java class

--- app/src/main/java/com/example/musicloud/persistence/stubs/SongPersistenceStub.java
+++ app/src/main/java/com/example/musicloud/persistence/stubs/SongPersistenceStub.java
@@ -12,19 +12,22 @@ import java.util.List;
 public class SongPersistenceStub implements SongPersistence {
     //list of all songs in users library
     private List<Song> songs;
+    //list of all liked songs
+    private List<Song> likedSongs;
 
-    public SongPersistenceStub(){
+    public SongPersistenceStub() {
         this.songs = new ArrayList<>();
 
         songs.add(new Song("Not Enough To Give", "Ketsa", "Ketsa - Not Enough To Give"));
         songs.add(new Song("Rain Man", "Ketsa", "Ketsa - Rain Man"));
-        songs.add(new Song("Above the Clouds", "Scott Holmes Music", "Scott Holmes Music - Above the Clouds.mp3"));
-        songs.add(new Song("Stereohada", "Nightfall", "Stereohada - Nightfall.mp3"));
+        songs.add(new Song("Above the Clouds", "Scott Holmes Music", "Above the Clouds.mp3"));
+        songs.add(new Song("Stereohada", "Nightfall", "Nightfall.mp3"));
     }
 
     @Override
     public List<Song> getAllSongs() {
-        return songs;
+
+        return Collections.unmodifiableList(songs);
     }
 
     @Override
@@ -48,8 +51,7 @@ public class SongPersistenceStub implements SongPersistence {
         int index;
 
         index = songs.indexOf(currentSong);
-        if (index >= 0)
-        {
+        if (index >= 0) {
             songs.set(index, currentSong);
         }
         return currentSong;
@@ -60,8 +62,7 @@ public class SongPersistenceStub implements SongPersistence {
         int index;
 
         index = songs.indexOf(currentSong);
-        if (index >= 0)
-        {
+        if (index >= 0) {
             songs.remove(index);
         }
     }
@@ -70,22 +71,44 @@ public class SongPersistenceStub implements SongPersistence {
     public List<String> allSongNames() {
         ArrayList<String> songNames = new ArrayList<>();
 
-        for(int i=0;i<songs.size();i++){
+        for (int i = 0; i < songs.size(); i++) {
             songNames.add(songs.get(i).getSongName());
         }
-
         return songNames;
     }
+
+    private static final String TAG = "SongPersistenceStub";
 
     @NonNull
     public String toString() {
         StringBuilder ans = new StringBuilder();
 
-        for(int i=0;i<songs.size();i++){
+        for (int i = 0; i < songs.size(); i++) {
             ans.append(songs.get(i)).append("\n");
         }
 
         ans.append("This is the summary of the songs object\nNumber of Songs:").append(songs.size());
         return ans.toString();
     }
+
+    @Override
+    public void likeSong(Song currentSong) {
+        currentSong.setLiked(true);
+        likedSongs.add(currentSong);
+    }
+
+    @Override
+    public void unlikeSong(Song currentSong) {
+        currentSong.setLiked(false);
+        int index = likedSongs.indexOf(currentSong);
+        if (index >= 0) {
+            likedSongs.remove(index);
+        }
+    }
+
+    @Override
+    public List<Song> getLikedSongs() {
+        return Collections.unmodifiableList(likedSongs);
+    }
+
 }
