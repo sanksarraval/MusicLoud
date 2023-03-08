cf88bf8d34c01caf2bec5fcc01f995a6632a2475
jacobseraspi <seraspij@myumanitoba.ca>
2023-03-08 16:18:53 -0600

    Combined new UI colours with like button functional

--- app/src/main/java/com/example/musicloud/persistence/hsqldb/SongPersistenceHSQLDB.java
+++ app/src/main/java/com/example/musicloud/persistence/hsqldb/SongPersistenceHSQLDB.java
@@ -175,6 +175,7 @@ public class SongPersistenceHSQLDB implements SongPersistence {
         String sql = "UPDATE table_song SET is_liked = ? WHERE id = ?";
         try(final Connection c = connection()) {
             PreparedStatement stmt = c.prepareStatement(sql);
+            currentSong.setLiked();
             stmt.setBoolean(1, true);
             stmt.setInt(2, currentSong.getId());
             stmt.executeUpdate();
@@ -188,12 +189,18 @@ public class SongPersistenceHSQLDB implements SongPersistence {
         String sql = "UPDATE table_song SET is_liked = ? WHERE id = ?";
         try (final Connection c = connection()){
             PreparedStatement stmt = c.prepareStatement(sql);
+            currentSong.setLiked();
             stmt.setBoolean(1, false);
             stmt.setInt(2, currentSong.getId());
             stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public boolean isLiked(Song currentSong) {
+        return currentSong.isLiked();
     }
 
     @Override
