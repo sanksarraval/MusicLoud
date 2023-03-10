28e24a9af9b83cd00d737015995cfc3c89ed34f8
logandoran <doranl1@myumanitoba.ca>
2023-03-10 09:50:26 -0600

    Revert "Deleting Playlist Java Files, Adding Thumbnails to the songs."
    This reverts commit 0f54388007e17ba4ce30ed921aad47cdbd641b64.

--- app/src/test/java/com/example/musicloud/AllTests.java
+++ app/src/test/java/com/example/musicloud/AllTests.java
@@ -1,11 +1,17 @@
 package com.example.musicloud;
 
 import com.example.musicloud.application.MediaPlayerTest;
+import com.example.musicloud.business.AccessPlaylistTest;
+import com.example.musicloud.business.AccessSPTest;
+import com.example.musicloud.business.AccessSongs;
 import com.example.musicloud.business.AccessSongsTest;
 import com.example.musicloud.business.AccessUsersTest;
 import com.example.musicloud.business.LoginManagerTest;
 import com.example.musicloud.business.ValidationInputTest;
+import com.example.musicloud.objects.PlaylistTest;
+import com.example.musicloud.objects.SPTest;
 import com.example.musicloud.objects.UserTest;
+import com.example.musicloud.persistence.HSQLDB.PlaylistPersistenceHSQLDBTest;
 import com.example.musicloud.persistence.UserManagementTest;
 import com.example.musicloud.persistence.HSQLDB.SongPersistenceHSQLDBTest;
 import com.example.musicloud.persistence.HSQLDB.UserManagementHSQLDBTest;
@@ -19,15 +25,16 @@ import org.junit.runners.Suite;
         MediaPlayerTest.class,
         UserManagementTest.class,
         SongPersistenceHSQLDBTest.class,
-
+        PlaylistPersistenceHSQLDBTest.class,
         UserManagementHSQLDBTest.class,
-
+        AccessPlaylistTest.class,
         AccessSongsTest.class,
-
+        AccessSPTest.class,
         AccessUsersTest.class,
         LoginManagerTest.class,
         ValidationInputTest.class,
-
+        PlaylistTest.class,
+        SPTest.class
 })
 public class AllTests
 {
