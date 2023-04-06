026db28fce8ac2b145d71b524ef83363c0e2510f
Aakash <chouhana@myumanitoba.ca>
2023-04-05 20:33:08 -0500

    Corrected  code to avoid repition of dummy playlist data

--- app/src/main/java/com/example/musicloud/persistence/hsqldb/PlaylistPersistenceHSQLDB.java
+++ app/src/main/java/com/example/musicloud/persistence/hsqldb/PlaylistPersistenceHSQLDB.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class PlaylistPersistenceHSQLDB implements PlaylistPersistence {
     private final String dbPath;
+    private static boolean wasCreated = false;
 
     /**
      * constructor
@@ -22,6 +23,14 @@ public class PlaylistPersistenceHSQLDB implements PlaylistPersistence {
         this.dbPath = dbPath;
         try{
             createTable();
+
+            if(wasCreated){
+
+            }
+            else{
+                wasCreated = true;
+                insertDummyData();
+            }
         }
         catch(SQLException e){
             e.printStackTrace();
