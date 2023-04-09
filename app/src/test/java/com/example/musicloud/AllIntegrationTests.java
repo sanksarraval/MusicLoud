ec613d05b106d35a0bf2df7a1b4a7d67833230c0
Jamster <langx@myumanitoba.ca>
2023-04-09 15:24:48 -0500

    update AllIntegrationTests

--- app/src/test/java/com/example/musicloud/AllIntegrationTests.java
+++ app/src/test/java/com/example/musicloud/AllIntegrationTests.java
@@ -2,14 +2,15 @@ package com.example.musicloud;
 
 import com.example.musicloud.application.MediaPlayerIntegrationTest;
 import com.example.musicloud.persistence.HSQLDB.SongPersistenceHSQLDBIntegrationTest;
-
+import com.example.musicloud.persistence.HSQLDB.UserManagementHSQLDBIntegrationTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         MediaPlayerIntegrationTest.class,
-        SongPersistenceHSQLDBIntegrationTest.class
+        SongPersistenceHSQLDBIntegrationTest.class,
+        UserManagementHSQLDBIntegrationTest.class
 })
 public class AllIntegrationTests {
 
